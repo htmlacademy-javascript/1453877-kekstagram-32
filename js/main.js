c83008927732d6af89f8ae176fe7211c897d7009
Philipp Prigonsky <jerk1991@gmail.com>
2024-06-27 12:53:03 +0300

    Добавил обработчики событий на миниатюры и кнопку закрытия полноэкранного просмотра

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
-
 import { NAMES, DESCRIPTIONS, MESSAGES } from './mocks.js';
 import { createArrayOfPhotoCards } from './data.js';
-import { makeThumbnails } from './gallery.js';
+import { showThumbnails } from './thumbnails.js';
+import { fillBigPicture } from './big-picture.js';
+import { toggleModal, isEscapeKey } from './functions.js';
 
-makeThumbnails(createArrayOfPhotoCards(25, MESSAGES, NAMES, DESCRIPTIONS));
+const picturesContent = createArrayOfPhotoCards(25, MESSAGES, NAMES, DESCRIPTIONS);
+showThumbnails(picturesContent);
+
+const picturesBlock = document.querySelector('.pictures');
+const bigPicture = document.querySelector('.big-picture');
+
+const onEscapeKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    toggleModal(bigPicture);
+    document.removeEventListener('keydown', onEscapeKeydown);
+    picturesBlock.addEventListener('click', onThumbnailClick);
+  }
+};
+
+const onThumbnailClick = (evt) => {
+  if (evt.target.closest('a')) {
+    const pictureBlock = evt.target.closest('a');
+    const closeBigPicture = bigPicture.querySelector('.cancel');
+    fillBigPicture(picturesContent[pictureBlock.dataset.id - 1], bigPicture);
+    toggleModal(bigPicture);
+    closeBigPicture.addEventListener('click', onBigPictureCloseClick);
+    picturesBlock.removeEventListener('click', onThumbnailClick);
+    document.addEventListener('keydown', onEscapeKeydown);
+  }
+};
+
+const onBigPictureCloseClick = () => {
+  toggleModal(bigPicture);
+  picturesBlock.addEventListener('click', onThumbnailClick);
+  document.removeEventListener('keydown', onEscapeKeydown);
+};
+
+picturesBlock.addEventListener('click', onThumbnailClick);
---
+++ js/thumbnails.js
@@ -0,0 +1,21 @@
+const fillThumbnail = (pictureContent, thumbnail) => {
+  thumbnail.querySelector('.picture__img').src = pictureContent.url;
+  thumbnail.querySelector('.picture__img').alt = pictureContent.description;
+  thumbnail.querySelector('.picture__comments').textContent = pictureContent.comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = pictureContent.likes;
+  thumbnail.querySelector('.picture').setAttribute('data-id', pictureContent.id);
+  return thumbnail;
+};
+
+const showThumbnails = (picturesContent) => {
+  const thumbnailTemplate = document.querySelector('#picture').content;
+  const picturesBlock = document.querySelector('.pictures');
+  const thumbnailsFragment = document.createDocumentFragment();
+  for (let currentThumbnail = 0; currentThumbnail < picturesContent.length; currentThumbnail++) {
+    const thumbnail = thumbnailTemplate.cloneNode(true);
+    thumbnailsFragment.appendChild(fillThumbnail(picturesContent[currentThumbnail], thumbnail));
+  }
+  picturesBlock.appendChild(thumbnailsFragment);
+};
+
+export { showThumbnails };
