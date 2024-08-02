e9c14072ddbb94d630fcb01f7c4506bde631c9f3
Philipp Prigonsky <jerk1991@gmail.com>
2024-08-02 22:38:04 +0300

    Вывел всё лишнее из main.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+import { showThumbnails } from './thumbnails.js';
+import { listenThumbnails } from './gallery.js';
+import { listenUploadForm } from './form.js';
+import { DefaultMainValues } from './const.js';
+import { showDataErrorAlert } from './validation.js';
+import { getData } from './api.js';
+import { listenFiltersBlock } from './filter.js';
+
+export const startApp = () => {
+  getData()
+    .then((picturesContent) => {
+      showThumbnails(picturesContent.slice(0, DefaultMainValues.PICTURES_TO_LOAD));
+      listenThumbnails(picturesContent);
+      listenFiltersBlock(picturesContent);
+    })
+    .catch(
+      (err) => {
+        showDataErrorAlert(err.message);
+      }
+    );
+  listenUploadForm();
+};
--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-import { showThumbnails } from './thumbnails.js';
-import { listenThumbnails } from './gallery.js';
-import { listenUploadForm } from './form.js';
-import { DefaultMainValues } from './const.js';
-import { showDataErrorAlert } from './validation.js';
-import { getData } from './api.js';
-import { listenFiltersBlock } from './filter.js';
+import { startApp } from './app';
 
-getData()
-  .then((picturesContent) => {
-    showThumbnails(picturesContent.slice(0, DefaultMainValues.PICTURES_TO_LOAD));
-    listenThumbnails(picturesContent);
-    listenFiltersBlock(picturesContent);
-  })
-  .catch(
-    (err) => {
-      showDataErrorAlert(err.message);
-    }
-  );
-
-listenUploadForm();
+startApp();
